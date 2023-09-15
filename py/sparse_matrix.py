dbcba5c3f9e600f26ae0a58c4109688209884120
danxalu <danxalu@gmail.com>
2023-09-15 19:59:10 +0300

    initial

--- py/sparse_matrix.py
+++ py/sparse_matrix.py
@@ -54,10 +54,11 @@ class SparseMatrix:
         for i in range(n):
             for element in self.data_[i]:
                 A[i, element.index] = element.value
+        print(self);
+        print(A);
         return A
 
     def __matmul__(self, other):
-
         # vvvvv your code here vvvvv
         result = SparseMatrix(dense=self.to_dense() @ other.to_dense())
         # ^^^^^ your code here ^^^^^
@@ -65,9 +66,58 @@ class SparseMatrix:
         return result
 
     def __pow__(self, power, modulo=None):
+        dense = self.to_dense();
 
         # vvvvv your code here vvvvv
-        result = SparseMatrix(dense=np.linalg.matrix_power(self.to_dense(), power))
+        ##result = SparseMatrix(dense=np.linalg.matrix_power(self.to_dense(), power)
+        dense_self = dense
+        dense_other = np.array(dense)
+        if (dense.shape[1] != dense.shape[0]):
+            print("Check rows and cols!")
+            return None
+        lenA = dense.shape[0]
+
+        dense_other = dense_other.transpose()
+
+        result = np.eye(lenA)
+
+        for apos in range(lenA):
+            r = dense_self[apos][0]
+            for bpos in range(lenA):
+                c = dense_other[bpos][0]
+                tempa = apos
+                tempb = bpos
+                sum_ = 0
+                while (tempa < lenA and dense_self[tempa][0] == r and tempb < lenA and dense_other[tempb][0] == c):
+
+                    if (dense_self[tempa][1] < dense_other[tempb][1]):
+                        tempa += 1
+                    elif (dense_self[tempa][1] > dense_other[tempb][1]):
+                        tempb += 1
+                    else:
+                        sum_ += dense_self[tempa][2] * dense_other[tempb][2]
+                        tempa += 1
+                        tempb += 1
+
+                if (sum_ != 0):
+                    print("sum", sum_)
+                    r = int(r)
+                    c = int(c)
+                    result[r][c] = sum_
+                while (bpos < lenA and dense_other[bpos][0] == c):
+                    bpos += 1
+
+            while (apos < lenA and dense_self[apos][0] == r):
+                apos += 1
         # ^^^^^ your code here ^^^^^
 
-        return result
+        ##result np.array
+        f = open("matr", "w")
+        f.write(lenA)
+        f.write("\n")
+        for i, j in result:
+            f.write("i, j")
+            f.write("\n")
+        f.close()
+
+        return result ##выводит np.array
